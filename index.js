922a2ba57c7d21dea8ead68affb36b642a162819
Petros Demetriou <dimitriupe@gmail.com>
2019-12-08 22:38:32 +0200

    Final commit for JS I project

--- index.js
+++ index.js
@@ -67,8 +67,9 @@ function processLength(/* CODE HERE */list, callback) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
+function processLastItem(/* CODE HERE */stringList, callback) {
   /* CODE HERE */
+  return callback(stringList[stringList.length - 1]);
 }
 
 
